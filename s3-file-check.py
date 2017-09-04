fc3a60c7600f4ea2d80077bca197a6b3098a2273
HK <harkamals@gmail.com>
2017-09-04 23:35:25 +0800

    s3-file-check.py

--- s3-file-check.py
+++ s3-file-check.py
@@ -8,6 +8,7 @@
 ####################################################################
 
 import os
+import json
 import datetime
 import boto3
 from botocore.errorfactory import ClientError
@@ -34,24 +35,29 @@ def send_sns_notification(message):
 today_prefix = (datetime.datetime.utcnow().strftime("%Y%m%d"))
 # today_prefix = "20170829"
 
-print("S3 file check for today: [%s]" % today_prefix)
-for bucket in ["dev-ds-syslogs", "dev-spark-syslogs"]:
-    print("Processing bucket: %s" % bucket)
 
-    try:
-        response = s3_client.list_objects_v2(Bucket=bucket, MaxKeys=5, Prefix=today_prefix)
-        if 'Contents' in response:
-            if [x for x in response['Contents']].__len__() > 0:
-                print("\tSystem normal: Bucket [%s] has received files for today" % bucket)
+def lambda_handler(event, context):
+    print('Debug: Event received:', json.dumps(event, indent=2))
+    print('Debug: Context vars:  ', vars(context))
+
+    print("S3 file check for today: [%s]" % today_prefix)
+    for bucket in ["dev-ds-syslogs", "dev-spark-syslogs"]:
+        print("Processing bucket: %s" % bucket)
+
+        try:
+            response = s3_client.list_objects_v2(Bucket=bucket, MaxKeys=5, Prefix=today_prefix)
+            if 'Contents' in response:
+                if [x for x in response['Contents']].__len__() > 0:
+                    print("\tSystem normal: Bucket [%s] has received files for today" % bucket)
+                else:
+                    send_sns_notification("Bucket [%s] has no files for today" % bucket)
             else:
-                send_sns_notification("Bucket [%s] has no files for today" % bucket)
-        else:
-            send_sns_notification("Bucket [%s] has no files for specified filter" % bucket)
+                send_sns_notification("Bucket [%s] has no files for specified filter" % bucket)
 
-    except ClientError as ce:
-        if ce.response['ResponseMetadata']['HTTPStatusCode'] == 404:
-            send_sns_notification("Bucket [%s] not found" % bucket)
-        else:
-            send_sns_notification("API [%s]" % ce.message)
-    except Exception as ex:
-        send_sns_notification("Unknown exception [%s]" % ex)
+        except ClientError as ce:
+            if ce.response['ResponseMetadata']['HTTPStatusCode'] == 404:
+                send_sns_notification("Bucket [%s] not found" % bucket)
+            else:
+                send_sns_notification("API [%s]" % ce.message)
+        except Exception as ex:
+            send_sns_notification("Unknown exception [%s]" % ex)
