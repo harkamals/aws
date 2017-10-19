f4d2e6f4f9e4b33f0e76408343467565041f5be8
HK <harkamals@gmail.com>
2017-10-19 23:17:52 +0800

    get ec2 metadata

--- src/cloudwatch/aws-cloudwatch-monitor/main.go
+++ src/cloudwatch/aws-cloudwatch-monitor/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/aws/aws-sdk-go/aws"
-	_ "github.com/aws/aws-sdk-go/service/cloudwatch"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
+	_, err := getMetaData()
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 }
